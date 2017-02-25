1f9e9738d49b25b8f4328fb360127cc5ad1a3c32
David Fox <me@iamdavidfox.com>
2017-02-24 22:15:04 -0500

    password tweak

--- PixelApp/App_Start/IdentityConfig.cs
+++ PixelApp/App_Start/IdentityConfig.cs
@@ -53,7 +53,7 @@ namespace PixelApp
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                //RequiredLength = 6,
+                RequiredLength = 4,
                 //RequireNonLetterOrDigit = true,
                 //RequireDigit = true,
                 //RequireLowercase = true,
