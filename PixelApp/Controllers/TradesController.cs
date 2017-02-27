7fb0042c73e157273d2321edcecb9f77acbc4a96
David Fox <me@iamdavidfox.com>
2017-02-27 09:10:03 -0500

    paring back scaffolded trade stuff

--- PixelApp/Controllers/TradesController.cs
+++ PixelApp/Controllers/TradesController.cs
@@ -14,36 +14,20 @@ namespace PixelApp.Controllers
     [Authorize]
     public class TradesController : BaseController
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
         // GET: Trades
         public async Task<ActionResult> Index()
         {
-            var trades = db.Trades.Include(t => t.Owner).Include(t => t.TradedToUser);
+            var trades = this.Context.Trades.Include(t => t.Owner).Include(t => t.TradedToUser);
             return View(await trades.ToListAsync());
-        }
-
-        // GET: Trades/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Trade trade = await db.Trades.FindAsync(id);
-            if (trade == null)
-            {
-                return HttpNotFound();
-            }
-            return View(trade);
         }
 
         // GET: Trades/Create
         public ActionResult Create()
         {
-            ViewBag.OwnerId = new SelectList(db.Users, "Id", "UserName");
-            ViewBag.TradedToUserId = new SelectList(db.Users, "Id", "UserName");
-            return View();
+            var vm = new Trade();
+            vm.OwnerId = this.UserContext.Id;
+
+            return View(vm);
         }
 
         // POST: Trades/Create
@@ -51,63 +35,45 @@ namespace PixelApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "TradeId,QuantityOffered,TypeOffered,QuantityAsked,TypeAsked,Posted,IsActive,OwnerId,TradedToUserId")] Trade trade)
+        public async Task<ActionResult> Create([Bind(Include = "TradeId,QuantityOffered,TypeOffered,QuantityAsked,TypeAsked,Posted,OwnerId")] Trade trade)
         {
             if (ModelState.IsValid)
             {
-                db.Trades.Add(trade);
-                await db.SaveChangesAsync();
+                this.Context.Trades.Add(trade);
+                await this.Context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.OwnerId = new SelectList(db.Users, "Id", "UserName", trade.OwnerId);
-            ViewBag.TradedToUserId = new SelectList(db.Users, "Id", "UserName", trade.TradedToUserId);
             return View(trade);
         }
 
-        // GET: Trades/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        // GET: Trades/Accept/5
+        public async Task<ActionResult> Accept(int? id)
         {
+            throw new NotImplementedException();
+
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Trade trade = await db.Trades.FindAsync(id);
+            Trade trade = await this.Context.Trades.FindAsync(id);
             if (trade == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.OwnerId = new SelectList(db.Users, "Id", "UserName", trade.OwnerId);
-            ViewBag.TradedToUserId = new SelectList(db.Users, "Id", "UserName", trade.TradedToUserId);
+            ViewBag.OwnerId = new SelectList(this.Context.Users, "Id", "UserName", trade.OwnerId);
+            ViewBag.TradedToUserId = new SelectList(this.Context.Users, "Id", "UserName", trade.TradedToUserId);
             return View(trade);
         }
 
-        // POST: Trades/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "TradeId,QuantityOffered,TypeOffered,QuantityAsked,TypeAsked,Posted,IsActive,OwnerId,TradedToUserId")] Trade trade)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(trade).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.OwnerId = new SelectList(db.Users, "Id", "UserName", trade.OwnerId);
-            ViewBag.TradedToUserId = new SelectList(db.Users, "Id", "UserName", trade.TradedToUserId);
-            return View(trade);
-        }
-
-        // GET: Trades/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        // GET: Trades/Cancel/5
+        public async Task<ActionResult> Cancel(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Trade trade = await db.Trades.FindAsync(id);
+            Trade trade = await this.Context.Trades.FindAsync(id);
             if (trade == null)
             {
                 return HttpNotFound();
@@ -115,14 +81,14 @@ namespace PixelApp.Controllers
             return View(trade);
         }
 
-        // POST: Trades/Delete/5
-        [HttpPost, ActionName("Delete")]
+        // POST: Trades/Cancel/5
+        [HttpPost, ActionName("Cancel")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public async Task<ActionResult> CancelConfirmed(int id)
         {
-            Trade trade = await db.Trades.FindAsync(id);
-            db.Trades.Remove(trade);
-            await db.SaveChangesAsync();
+            Trade trade = await this.Context.Trades.FindAsync(id);
+            trade.IsActive = false;
+            await this.Context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
@@ -130,7 +96,7 @@ namespace PixelApp.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                this.Context.Dispose();
             }
             base.Dispose(disposing);
         }
