52a34719f0f6f202f5bb3128f58e34917e69626f
David Fox <me@iamdavidfox.com>
2017-03-03 23:10:03 -0500

    notification management by users

---
+++ PixelApp/Controllers/MailboxController.cs
@@ -0,0 +1,70 @@
+﻿using PixelApp.Views.Mailbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PixelApp.Controllers
+{
+    public class MailboxController : BaseController
+    {
+        public JsonResult Any()
+        {
+            var notes = this.Context.Notes.Any(x => 
+                x.UserId == this.UserContext.Id
+                && x.IsActive.Equals(true) 
+                && x.IsRead.Equals(false));
+
+            return Json(new { hasmail = notes }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            var vm = new MailboxViewModel();
+
+            vm.Notes = this.Context.Notes
+                .Where(x => x.UserId == this.UserContext.Id)
+                .Where(x => x.IsActive.Equals(true))
+                .Select(x => new NoteSkinny
+                {
+                    IsRead = x.IsRead,
+                    NoteId = x.NoteId,
+                    Sent = x.Sent,
+                    Body = x.Body,
+                    Title = x.Title,
+                })
+                .ToList();
+
+            return View(vm);
+        }
+
+        public ActionResult View(int id)
+        {
+            var note = this.Context.Notes.Single(x => x.NoteId == id);
+
+            if(note.UserId != this.UserContext.Id)
+            {
+                return HttpNotFound();
+            }
+
+            note.IsRead = true;
+
+            return View(note);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var note = this.Context.Notes.Single(x => x.NoteId == id);
+
+            if (note.UserId != this.UserContext.Id)
+            {
+                return HttpNotFound();
+            }
+
+            note.IsActive = false;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PixelApp/Views/Mailbox/Models/MailboxViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PixelApp.Views.Mailbox.Models
+{
+    public class MailboxViewModel
+    {
+        public List<NoteSkinny> Notes { get; set; }
+
+        public MailboxViewModel()
+        {
+            this.Notes = new List<NoteSkinny>();
+        }
+    }
+
+    public class NoteSkinny
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Sent { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
