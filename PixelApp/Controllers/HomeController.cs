c4007450e8d81265788fc5706ec96f9dc30e7b89
David Fox <me@iamdavidfox.com>
2017-03-04 06:41:32 -0500

    clean up

--- PixelApp/Controllers/HomeController.cs
+++ PixelApp/Controllers/HomeController.cs
@@ -112,19 +112,5 @@ namespace PixelApp.Controllers
                 ModelState.AddModelError("", error);
             }
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
