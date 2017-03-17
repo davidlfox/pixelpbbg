18bed98749c49e914e336e4767900066deeb9e4e
David Fox <me@iamdavidfox.com>
2017-03-16 23:11:44 -0400

    show some inventory

---
+++ PixelApp/Controllers/InventoryController.cs
@@ -0,0 +1,21 @@
+﻿using PixelApp.Views.Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PixelApp.Controllers
+{
+    [Authorize]
+    public class InventoryController : BaseController
+    {
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            var vm = new InventoryIndexViewModel();
+            vm.LoadItems(this.Context, this.UserContext, this.UserContext.Territory.CivilianPopulation);
+            return View(vm);
+        }
+    }
+}
