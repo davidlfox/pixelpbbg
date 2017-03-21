1bd14dfc78ffa56656bb1b6c7f41826e7d8dc9e6
Marty Goon <martingoon@gmail.com>
2017-03-20 23:38:51 -0400

    Added map links to dash

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,5 @@
 select * from AspNetUsers where Email='b@b.com'
-update AspNetUsers set EmailConfirmed = 1 where Email='b@b.com' 
+update AspNetUsers set EmailConfirmed = 1 where Email='c@c.com' 
 select * from territories
 delete from territories where Name like '%testing%'
 drop database [aspnet-PixelApp-20170207052120]
