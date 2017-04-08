c1b8fcd6d0e9f24fa631883c8df2ea4cdd2437af
David Fox <me@iamdavidfox.com>
2017-04-08 11:00:22 -0400

    wire up impersonation

--- PixelApp/Controllers/ImpersonateController.cs
+++ PixelApp/Controllers/ImpersonateController.cs
@@ -1,19 +1,67 @@
-﻿using Pixel.Common;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using Pixel.Common;
+using PixelApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace PixelApp.Controllers
 {
-    [Authorize(Roles = Permissions.CanImpersonateUsers)]
     public class ImpersonateController : BaseController
     {
-        // GET: Impersonate
-        public ActionResult Index()
+        [Authorize(Roles = Permissions.CanImpersonateUsers)]
+        public async Task<ActionResult> StartAsync(string userName)
         {
-            return View();
+            var context = System.Web.HttpContext.Current;
+
+            var originalUsername = context.User.Identity.Name;
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            var impersonatedUser = await userManager.FindByNameAsync(userName);
+
+            var impersonatedIdentity = await userManager.CreateIdentityAsync(impersonatedUser, DefaultAuthenticationTypes.ApplicationCookie);
+            impersonatedIdentity.AddClaim(new Claim("UserImpersonation", "true"));
+            impersonatedIdentity.AddClaim(new Claim("OriginalUsername", originalUsername));
+
+            var authenticationManager = context.GetOwinContext().Authentication;
+            authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, impersonatedIdentity);
+
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        [Authorize]
+        public async Task<ActionResult> StopAsync()
+        {
+            if (System.Web.HttpContext.Current.User.IsImpersonating())
+            {
+                var context = System.Web.HttpContext.Current;
+
+                var originalUsername = context.User.GetOriginalUsername();
+
+                var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+                var originalUser = await userManager.FindByNameAsync(originalUsername);
+
+                var impersonatedIdentity = await userManager.CreateIdentityAsync(originalUser, DefaultAuthenticationTypes.ApplicationCookie);
+                var authenticationManager = context.GetOwinContext().Authentication;
+
+                authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, impersonatedIdentity);
+            }
+            else
+            {
+                throw new Exception("Unable to remove impersonation because there is no impersonation");
+            }
+
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
---
+++ PixelApp/Models/IdentityExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace PixelApp.Models
+{
+    public static class IdentityExtensions
+    {
+        public static bool IsImpersonating(this IPrincipal principal)
+        {
+            if (principal == null)
+            {
+                return false;
+            }
+
+            var claimsPrincipal = principal as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+            {
+                return false;
+            }
+
+            return claimsPrincipal.HasClaim("UserImpersonation", "true");
+        }
+
+        public static String GetOriginalUsername(this IPrincipal principal)
+        {
+            if (principal == null)
+            {
+                return String.Empty;
+            }
+
+            var claimsPrincipal = principal as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+            {
+                return String.Empty;
+            }
+
+            if (!claimsPrincipal.IsImpersonating())
+            {
+                return String.Empty;
+            }
+
+            var originalUsernameClaim = claimsPrincipal.Claims.SingleOrDefault(c => c.Type == "OriginalUsername");
+
+            if (originalUsernameClaim == null)
+            {
+                return String.Empty;
+            }
+
+            return originalUsernameClaim.Value;
+        }
+    }
+}
