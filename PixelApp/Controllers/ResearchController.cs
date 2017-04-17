7ecd0c5b5e920905a2ca762800fd20fa5436fd22
David Fox <me@iamdavidfox.com>
2017-04-16 22:37:46 -0400

    more ajax malarkey

--- PixelApp/Controllers/ResearchController.cs
+++ PixelApp/Controllers/ResearchController.cs
@@ -3,6 +3,7 @@ using PixelApp.Services;
 using PixelApp.Views.Research.Models;
 using System.Linq;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace PixelApp.Controllers
 {
@@ -34,15 +35,17 @@ namespace PixelApp.Controllers
         {
             var ts = new TechnologyService();
 
+            var userId = this.User.Identity.GetUserId();
+
             // Check for pending Research
-            var pending = ts.GetCheckPendingResearch(this.UserContext.Id);
+            var pending = ts.GetCheckPendingResearch(userId);
             ts.SaveChanges();
 
             if (pending != null)
                 return Json(new { success = false, message = string.Format("There is research currently pending ({0}) which must finish before starting new research.", pending.Technology.Name) }, JsonRequestBehavior.DenyGet);
 
             // Start Researching
-            var response = ts.StartResearch(technologyId, this.UserContext.Id);
+            var response = ts.StartResearch(technologyId, userId);
             if (response.IsSuccessful)
                 ts.SaveChanges();
 
